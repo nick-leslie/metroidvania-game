6e00bf62bd7a290680d51cd70361ce1bc588ffba
swordcom367 <46449945+swordcom367@users.noreply.github.com>
2019-09-19 08:37:47 -0400

    added the game and jump and move

---
+++ metroidvania game/Assets/Scripts/Player/Jump.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float fallMultiplayer=2.5f;
+    [SerializeField]
+    private float LowJumpMultiplyer=2;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplayer - 1)  * Time. deltaTime;
+        } else if( rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (LowJumpMultiplyer - 1) * Time.deltaTime;
+        }
+    }
+}
---
+++ metroidvania game/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    protected float playerInput;
+    [SerializeField] private float sprintSpeed;
+    [SerializeField] private float walkspeed;
+    private float movespeed;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInput = Input.GetAxis("Horizontal");
+        movespeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkspeed;
+
+        rb.velocity = new Vector2 (Input.GetAxisRaw("Horizontal") * movespeed* Time.deltaTime,rb.velocity.y);
+    }
+}
