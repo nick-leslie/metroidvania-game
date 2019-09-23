76266b5b7b183ab8cf6e1149c374d5c0f3c29632
swordcom367 <46449945+swordcom367@users.noreply.github.com>
2019-09-23 15:58:59 -0400

    added functions to healing

---
+++ metroidvania game/Assets/Scripts/Player/heal.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class heal : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject insidecercal;
+    private GameObject shrinkingcircal;
+    private RectTransform insidecircalRectTransform;
+    private RectTransform shrinkingcircalRectTransform;
+    private GameObject player;
+    private bool healing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shrinkingcircal = gameObject;
+        shrinkingcircalRectTransform = gameObject.GetComponent<RectTransform>();
+        insidecircalRectTransform = insidecercal.GetComponent<RectTransform>();
+        insidecercal.GetComponent<Image>().enabled = false;
+        shrinkingcircal.GetComponent<Image>().enabled = false;
+        shrinkingcircal.GetComponent<circalShrinking>().enabled = false;
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(shrinkingcircalRectTransform.sizeDelta.x<=insidecircalRectTransform.sizeDelta.x && shrinkingcircalRectTransform.sizeDelta.y <= insidecircalRectTransform.sizeDelta.y)
+        {
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                //todo
+                healingState(healing);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.F) && healing==false)
+            {
+                healingState(healing);
+            }
+            else if (Input.GetKeyDown(KeyCode.F))
+            {
+                healingState(healing);
+            }
+        }
+    }
+    public void  healingState(bool state)
+    {
+        if (state == true)
+        {
+            player.GetComponent<Movement>().enabled = true;
+            player.GetComponent<Jump>().enabled = true;
+            insidecercal.GetComponent<Image>().enabled = false;
+            shrinkingcircal.GetComponent<Image>().enabled = false;
+            shrinkingcircal.GetComponent<circalShrinking>().enabled = false;
+            shrinkingcircalRectTransform.sizeDelta = gameObject.GetComponent<circalShrinking>().startsize;
+            healing = false;
+        }
+        else
+        {
+            insidecercal.GetComponent<Image>().enabled = true;
+            shrinkingcircal.GetComponent<Image>().enabled = true;
+            shrinkingcircal.GetComponent<circalShrinking>().enabled = true;
+            player.GetComponent<Movement>().enabled = false;
+            player.GetComponent<Jump>().enabled = false;
+            healing = true;
+        }
+    }
+}
---
+++ metroidvania game/Assets/Scripts/circalShrinking.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class circalShrinking : MonoBehaviour
+{
+    private RectTransform circal;
+    [SerializeField]
+    private float time;
+    public Vector2 startsize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        circal = gameObject.GetComponent<RectTransform>();
+        startsize = circal.sizeDelta;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        circal.sizeDelta = new Vector2(Mathf.Lerp(circal.sizeDelta.x, 0, time * Time.deltaTime), Mathf.Lerp(circal.sizeDelta.y, 0, time * Time.deltaTime));
+        if (circal.sizeDelta.x <=100 && circal.sizeDelta.y <= 100)
+        {
+            circal.sizeDelta = startsize;
+        }
+    }
+}
